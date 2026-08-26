05bf443f47a97d6a82524663d8a4f3bb122f15f6
Salim Nassim <salim.nassim@gmail.com>
2026-08-26 11:38:11 +0200

    add: encode

---
+++ encode.go
@@ -0,0 +1,114 @@
+package rtorrent
+
+import (
+	"bytes"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+// encodeMethodCall serializes an XML-RPC methodCall.
+func encodeMethodCall(name string, params []Value) []byte {
+	var buf bytes.Buffer
+
+	buf.WriteString(`<?xml version="1.0"?>`)
+	buf.WriteString("<methodCall><methodName>")
+	buf.WriteString(escapeXML(name))
+	buf.WriteString("</methodName><params>")
+
+	for _, p := range params {
+		buf.WriteString("<param>")
+		encodeValue(&buf, p)
+		buf.WriteString("</param>")
+	}
+
+	buf.WriteString("</params></methodCall>")
+
+	return buf.Bytes()
+}
+
+// encodeValue writes v to buf as an XML-RPC <value> element. KindInt64
+// emits <i8>, not <i4>.
+func encodeValue(buf *bytes.Buffer, v Value) {
+	buf.WriteString("<value>")
+
+	switch v.Kind() {
+	case KindString:
+		buf.WriteString("<string>")
+		buf.WriteString(escapeXML(v.str))
+		buf.WriteString("</string>")
+
+	case KindInt:
+		buf.WriteString("<i4>")
+		buf.WriteString(strconv.FormatInt(v.num, 10))
+		buf.WriteString("</i4>")
+
+	case KindInt64:
+		buf.WriteString("<i8>")
+		buf.WriteString(strconv.FormatInt(v.num, 10))
+		buf.WriteString("</i8>")
+
+	case KindDouble:
+		buf.WriteString("<double>")
+		buf.WriteString(strconv.FormatFloat(v.dbl, 'f', -1, 64))
+		buf.WriteString("</double>")
+
+	case KindBool:
+		buf.WriteString("<boolean>")
+		if v.b {
+			buf.WriteString("1")
+		} else {
+			buf.WriteString("0")
+		}
+		buf.WriteString("</boolean>")
+
+	case KindNil:
+		buf.WriteString("<nil/>")
+
+	case KindArray:
+		buf.WriteString("<array><data>")
+		for _, e := range v.arr {
+			encodeValue(buf, e)
+		}
+		buf.WriteString("</data></array>")
+
+	case KindStruct:
+		buf.WriteString("<struct>")
+
+		// Sorted for deterministic wire output.
+		keys := make([]string, 0, len(v.strct))
+		for k := range v.strct {
+			keys = append(keys, k)
+		}
+		sort.Strings(keys)
+		for _, k := range keys {
+			buf.WriteString("<member><name>")
+			buf.WriteString(escapeXML(k))
+			buf.WriteString("</name>")
+			encodeValue(buf, v.strct[k])
+			buf.WriteString("</member>")
+		}
+		buf.WriteString("</struct>")
+	}
+
+	buf.WriteString("</value>")
+}
+
+// escapeXML escapes the characters that are unsafe in XML element text
+// content.
+func escapeXML(s string) string {
+	var b strings.Builder
+	for _, r := range s {
+		switch r {
+		case '&':
+			b.WriteString("&amp;")
+		case '<':
+			b.WriteString("&lt;")
+		case '>':
+			b.WriteString("&gt;")
+		default:
+			b.WriteRune(r)
+		}
+	}
+	return b.String()
+}
---
+++ encode_test.go
@@ -0,0 +1,88 @@
+package rtorrent
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestEncodeValue(t *testing.T) {
+	tests := []struct {
+		name  string
+		value Value
+		want  string
+	}{
+		{name: "string", value: NewString("bar"), want: "<value><string>bar</string></value>"},
+		{
+			name:  "string escaping",
+			value: NewString("<a & b>"),
+			want:  "<value><string>&lt;a &amp; b&gt;</string></value>",
+		},
+		{name: "int", value: NewInt(42), want: "<value><i4>42</i4></value>"},
+		{name: "negative int", value: NewInt(-7), want: "<value><i4>-7</i4></value>"},
+		{
+			name:  "int64 emits i8, not i4",
+			value: NewInt64(9223372036854775807),
+			want:  "<value><i8>9223372036854775807</i8></value>",
+		},
+		{name: "double", value: NewDouble(3.25), want: "<value><double>3.25</double></value>"},
+		{name: "bool true", value: NewBool(true), want: "<value><boolean>1</boolean></value>"},
+		{name: "bool false", value: NewBool(false), want: "<value><boolean>0</boolean></value>"},
+		{name: "nil", value: NewNil(), want: "<value><nil/></value>"},
+		{
+			name:  "empty array",
+			value: NewArray(nil),
+			want:  "<value><array><data></data></array></value>",
+		},
+		{
+			name:  "array",
+			value: NewArray([]Value{NewInt(1), NewInt(2)}),
+			want:  "<value><array><data><value><i4>1</i4></value><value><i4>2</i4></value></data></array></value>",
+		},
+		{
+			name:  "struct single member",
+			value: NewStruct(map[string]Value{"a": NewInt(1)}),
+			want:  "<value><struct><member><name>a</name><value><i4>1</i4></value></member></struct></value>",
+		},
+		{
+			name:  "struct sorts members for deterministic output",
+			value: NewStruct(map[string]Value{"b": NewInt(2), "a": NewInt(1)}),
+			want: "<value><struct>" +
+				"<member><name>a</name><value><i4>1</i4></value></member>" +
+				"<member><name>b</name><value><i4>2</i4></value></member>" +
+				"</struct></value>",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var buf bytes.Buffer
+			encodeValue(&buf, tt.value)
+			if got := buf.String(); got != tt.want {
+				t.Errorf("encodeValue(%v) = %s, want %s", tt.value, got, tt.want)
+			}
+		})
+	}
+}
+
+func TestEncodeMethodCall(t *testing.T) {
+	got := string(encodeMethodCall("d.hash", []Value{NewString(""), NewString("d.hash=")}))
+	want := `<?xml version="1.0"?>` +
+		"<methodCall><methodName>d.hash</methodName><params>" +
+		"<param><value><string></string></value></param>" +
+		"<param><value><string>d.hash=</string></value></param>" +
+		"</params></methodCall>"
+
+	if got != want {
+		t.Errorf("encodeMethodCall() = %s, want %s", got, want)
+	}
+}
+
+func TestEncodeMethodCallNoParams(t *testing.T) {
+	got := string(encodeMethodCall("system.listMethods", nil))
+	want := `<?xml version="1.0"?>` +
+		"<methodCall><methodName>system.listMethods</methodName><params></params></methodCall>"
+
+	if got != want {
+		t.Errorf("encodeMethodCall() = %s, want %s", got, want)
+	}
+}
